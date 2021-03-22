67e9d68026507a8cdedf7a2ed96862f75f560e9d
QP <bmcciscoding@gmail.com>
2021-03-22 11:46:01 +0800

    init apple app

---
+++ FlutterNotes/lib/app.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'styles.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
+
+// class CupertinoStoreHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return CupertinoPageScaffold(
+//       navigationBar: CupertinoNavigationBar(
+//         middle: const Text('Cupertino Store'),
+//       ),
+//       child: Container(),
+//     );
+//   }
+// }
+
+class CupertinoStoreHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.arrow_up_right_diamond),
+            title: Text('Products'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            title: Text('Search'),
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+          case 1:
+            return CupertinoTabView(builder: (context) {
+              return CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+        }
+      },
+    );
+  }
+}
+
+class SearchTab extends StatefulWidget {
+  @override
+  _SearchTabState createState() {
+    return _SearchTabState();
+  }
+}
+
+class _SearchTabState extends State<SearchTab> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: const <Widget>[
+        CupertinoSliverNavigationBar(
+          largeTitle: Text('Search'),
+        ),
+      ],
+    );
+  }
+}
